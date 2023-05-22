2aeddc5944a95f327a16d9cd71b16e0a4d6192be
armandoAndes <j.ardilab@uniandes.edu.co>
2023-05-22 10:04:49 -0500

    FEAT(H-000): routing and modules.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'register-user',
+    loadChildren: () => import('./register-user/register-user.module').then((m) => m.RegisterUserModule)
+  },
+  {
+    path: 'books',
+    loadChildren: () => import('./books/books-routing.module').then((m) => m.BooksRoutingModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login-routing.module').then((m) => m.LoginRoutingModule)
+  },
+  {
+    path: '',
+    redirectTo: '/login/login-form',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,32 @@
-import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
+import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
+import { AppComponent } from './app.component';
+
+import { AppRoutingModule } from './app-routing.module';
+import { RegisterUserModule } from './register-user/register-user.module';
+import { LoginModule } from './login/login.module';
+import { BooksModule } from './books/books.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule,
+    LoginModule,
+    RegisterUserModule,
+    BooksModule,
+    HttpClientModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
 })
 export class AppModule { }
